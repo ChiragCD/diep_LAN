541efae8a7eb246d36c6b49fd7d8eaf57d766377
ChiragCD <Chirag8CD@gmail.com>
2019-06-27 13:22:32 +0530

    Start working with input
    Set up the input system.

---
+++ Projects/Hello World/sprite.py
@@ -0,0 +1,45 @@
+import pygame
+
+class sprite(object):
+
+    def __init__(self):
+
+        pass
+
+class tank(object):
+
+    def __init__(self):
+
+        sprite.__init__(self)
+
+    def move_up(self):
+
+        pass
+
+    def move_down(self):
+
+        pass
+
+    def move_left(self):
+
+        pass
+
+    def move_right(self):
+
+        pass
+
+    def shoot(self):
+
+        pass
+
+    def autoshoot(self):
+
+        pass
+
+    def rotate_clockwise(self):
+
+        pass
+
+    def rotate_anticlockwise(self):
+
+        pass
