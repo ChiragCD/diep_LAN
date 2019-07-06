9659a812ac43845143a4e728905f39a9889519d7
ChiragCD <Chirag8CD@gmail.com>
2019-07-06 19:44:34 +0530

    switch to python 3.7
    asyncio in python3.7 appears, unfortunately, to have poor compatibility with pygame clock.tick method, forcing us to simulate a clock. As a result, the functions are written as regular functions (not coroutines), and have callbacks to themselves, on the given loop.

--- Projects/Hello World/client/main.py
+++ Projects/Hello World/client/main.py
@@ -1,4 +1,4 @@
-import pygame, asyncio, sprite
+import asyncio, pygame, sprite
 from input_config import action
 
 class program(object):
@@ -7,72 +7,64 @@ class program(object):
 
         pygame.init()
         self.tank = sprite.tank(0, 900, 900, 0)
+        self.screen = pygame.display.set_mode((500, 500))
+        self.back = pygame.image.load("back.jpeg").convert()
 
     def end(self):
-
+        
         pygame.quit()
+        quit()
 
-    @asyncio.coroutine
-    def main(self):
+    async def main(self):
 
         self.own_tank = pygame.sprite.Group()
         self.own_tank.add(self.tank)
         self.groups = [self.own_tank]
-        asyncio.ensure_future(self.output(), loop = loop)
-        asyncio.ensure_future(self.logic())
-        asyncio.ensure_future(self.input_handler(), loop = loop)
+        
+        loop = asyncio.get_running_loop()
+        loop.call_soon(self.output())
+        loop.call_soon(self.logic())
+        loop.call_soon(self.input_handler())
+
             ## Processing
             ## Output
             ## etc.
 
-    @asyncio.coroutine
     def input_handler(self):
+        
+        loop.call_at(loop.time() + 0.01, self.input_handler)
+        for event in pygame.event.get():
+            if(event.type == pygame.KEYDOWN):
+                action[event.key](self.tank, event.key, 1)     ## 1 represents key down
+                continue
+            if(event.type == pygame.KEYUP):
+                action[event.key](self.tank, event.key, 0)     ## 0 represents key up
+                continue
+            if(event.type == pygame.MOUSEBUTTONDOWN):
+                action[event.button](self.tank, 1)  ## 1 represents button pressed
+                continue
+            if(event.type == pygame.MOUSEBUTTONUP):
+                action[event.button](self.tank, 0)  ## 0 represents button released
+                continue
+            if(event.type == pygame.QUIT):
+                self.end()
+        return
 
-        clock = pygame.time.Clock()
-
-        while(True):
-            clock.tick(100)
-            for event in pygame.event.get():
-                if(event.type == pygame.KEYDOWN):
-                    action[event.key](self.tank, event.key, 1)     ## 1 represents key down
-                    continue
-                if(event.type == pygame.KEYUP):
-                    action[event.key](self.tank, event.key, 0)     ## 0 represents key up
-                    continue
-                if(event.type == pygame.MOUSEBUTTONDOWN):
-                    action[event.button](self.tank, 1)  ## 1 represents button pressed
-                    continue
-                if(event.type == pygame.MOUSEBUTTONUP):
-                    action[event.button](self.tank, 0)  ## 0 represents button released
-                    continue
-                if(event.type == pygame.QUIT):
-                    self.end()
-            yield
-
-    @asyncio.coroutine
     def logic(self):
 
-        while(True):
-            self.tank.move()
-            yield
-        
-    @asyncio.coroutine
+        loop.call_at(loop.time() + 0.02, self.logic)
+        self.tank.move()
+        return
+
     def output(self):
 
-        self.screen = pygame.display.set_mode((500, 500))
-        back = pygame.image.load("back.jpeg").convert()
+        loop.call_at(loop.time() + 0.02, self.output)
+        self.screen.blit(self.back, (self.tank.field_of_view - self.tank.pos_x, self.tank.field_of_view - self.tank.pos_y))
+        for group in self.groups:
+            group.draw(self.screen)
+        pygame.display.flip()
 
-        clock = pygame.time.Clock()
-
-        while(True):
-            clock.tick(60)
-            self.screen.blit(back, (self.tank.field_of_view - self.tank.pos_x, self.tank.field_of_view - self.tank.pos_y))
-            for group in self.groups:
-                group.draw(self.screen)
-            pygame.display.flip()
-            yield
-
-tester = program()
 loop = asyncio.get_event_loop()
-asyncio.ensure_future(tester.main(), loop = loop)
+tester = program()
+loop.create_task(tester.main())
 loop.run_forever()
