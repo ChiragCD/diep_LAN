415af5a8b133f30aac235746b0cd728b5f78c729
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-07-07 13:26:28 +0530

    Add code to draw a bullet
    Currently the code to draw a bullet contains some hard-coded values,
    which would later on be replaced by the properties of the bullet itself

--- Projects/Hello World/client/draw_sprite.py
+++ Projects/Hello World/client/draw_sprite.py
@@ -10,10 +10,19 @@ def test_tank():
     pygame.draw.circle(surface, data[0]["colour"], (data[0]["radius"], data[0]["radius"]), data[0]["radius"])
     return surface
 
+# This function is used to draw the bullet. Currently the color is hard-coded, will have to figure a way to make it available through parameters
+def bullet() :
+    surface = pygame.Surface((35, 35))
+    surface.fill((255, 255, 255))
+    surface.set_colorkey((255, 255, 255))
+    pygame.draw.circle(surface, ((0, 0, 255)), (data[201]["radius"], data[201]["radius"]), data[201]["radius"])
+    return surface
+
 def get_drawing(Type):
 
     drawings = {
-        0 : test_tank
+        0 : test_tank,
+        201 : bullet,
         }
 
     return drawings[Type]()
